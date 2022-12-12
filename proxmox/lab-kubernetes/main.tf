bd26f9d0e27fd9acce38026b5a2631b1fb1ebc2d
Leonardo Freitas <leonardofreitas.info@gmail.com>
2022-12-11 22:22:07 -0300

    Modify resouce name to k8s-master

--- proxmox/lab-kubernetes/main.tf
+++ proxmox/lab-kubernetes/main.tf
@@ -1,7 +1,7 @@
 # resource is formatted to be "[type]" "[entity_name]" so in this case
 # we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "docker-vm" {
-  count = 1 # just want 1 for now, set to 0 and apply to destroy VM
+  count = 4 # just want 1 for now, set to 0 and apply to destroy VM
   name = "vm-docker-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
   target_node = var.proxmox_host
@@ -19,7 +19,7 @@ resource "proxmox_vm_qemu" "docker-vm" {
   disk {
     slot = 0
     # set disk size here. leave it small for testing because expanding the disk takes time.
-    size = "10G"
+    size = "20G"
     type = "scsi"
     storage = "datastore"
     iothread = 1
@@ -47,21 +47,21 @@ resource "proxmox_vm_qemu" "docker-vm" {
   # in this case, since we are only adding a single VM, the IP will
   # be 10.98.1.91 since count.index starts at 0. this is how you can create
   # multiple VMs and have an IP assigned to each (.91, .92, .93, etc.)
-  ipconfig0 = "ip=10.10.100.7${count.index + 1}/24,gw=10.10.100.1"
+  ipconfig0 = "ip=10.10.100.5${count.index + 1}/24,gw=10.10.100.1"
   
   # sshkeys set using variables. the variable contains the text of the key.
   sshkeys = <<EOF
   ${var.ssh_key}
   EOF
   
-  provisioner "remote-exec" {
-    inline = [
-    "sudo apt update -y",
-    "sudo apt install -y ca-certificates curl gnupg lsb-release",
-    "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-    "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu   focal stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-    "sudo apt update -y",
-    "sudo apt install -y docker-ce docker-ce-cli containerd.io" 
-    ]
-  }
+#  provisioner "remote-exec" {
+#    inline = [
+#    "sudo apt update -y",
+#    "sudo apt install -y ca-certificates curl gnupg lsb-release",
+#    "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+#    "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu   focal stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+#    "sudo apt update -y",
+#    "sudo apt install -y docker-ce docker-ce-cli containerd.io" 
+#    ]
+#  }
 }
