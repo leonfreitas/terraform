b48c59bc5c62d389cb92cdee182b0d7e0f28bb77
Leonardo Freitas <leonardofreitas.info@gmail.com>
2023-01-05 19:32:34 -0300

    add local_file to generate the linux hosts file

--- proxmox/lab-kubernetes/output.tf
+++ proxmox/lab-kubernetes/output.tf
@@ -1,4 +1,4 @@
-resource "local_file" "hosts" {
+resource "local_file" "hosts_ansible" {
     filename = "./ansible/hosts.ini"
     content = <<-EOT
 [masters]
@@ -8,6 +8,32 @@ ${ip}
 [workers]
 %{for ip in proxmox_vm_qemu.k8s-worker.*.default_ipv4_address ~}
 ${ip}
+%{ endfor ~}
+EOT
+}
+
+resource "local_file" "hosts_linux" {
+    filename = "./hosts"
+    content = <<-EOT
+# Your system has configured 'manage_etc_hosts' as True.
+# As a result, if you wish for changes to this file to persist
+# then you will need to either
+# a.) make changes to the master file in /etc/cloud/templates/hosts.debian.tmpl
+# b.) change or remove the value of 'manage_etc_hosts' in
+#     /etc/cloud/cloud.cfg or cloud-config from user-data
+#
+127.0.0.1 localhost
+
+# The following lines are desirable for IPv6 capable hosts
+::1 localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+
+%{for ip in proxmox_vm_qemu.k8s-master.* ~}
+${ip.default_ipv4_address} ${ip.name} ${ip.name} 
+%{ endfor ~}
+%{for ip in proxmox_vm_qemu.k8s-worker.* ~}
+${ip.default_ipv4_address} ${ip.name} ${ip.name} 
 %{ endfor ~}
 EOT
 }
