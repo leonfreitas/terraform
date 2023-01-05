304dc4b3664dfe2335efcb15bad8ae5997df27da
Leonardo Freitas <leonardofreitas.info@gmail.com>
2023-01-05 19:30:06 -0300

    add local-exec to call to ansible playbook

--- proxmox/lab-kubernetes/provisioner.tf
+++ proxmox/lab-kubernetes/provisioner.tf
@@ -6,8 +6,8 @@
 
 resource "null_resource" "config" {
     provisioner "local-exec" {
-        command = "ansible -i ./ansible/hosts.ini all -m ping"
+        command = "ansible-playbook -i ./ansible/hosts.ini ./ansible/play.yml"
     }
-    #depends_on = [local_file.hosts,time_sleep.wait_30_seconds]
-    depends_on = [local_file.hosts]
+
+    depends_on = [local_file.hosts_ansible]
 }
